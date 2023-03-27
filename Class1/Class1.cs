a867e4722d69518f52af05c7558b7d3e61090d13
itsfauzan <fauzanyahya@students.amikom.ac.id>
2023-03-27 22:40:50 +0700

    class and object

---
+++ Class1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+public class Mobil
+{
+    //propertis
+    public string Warna { get; set; }
+    public int JumlahPintu { get; set; }
+    public string Merk { get; set; }
+    public string Model { get; set; }
+    public int TahunKeluar { get; set; }
+
+    //method
+    public void Gas(int kecepatan)
+    {
+        Console.WriteLine("Mobil {0} berjalan dengan kecepatan {1}", Model, kecepatan);
+    }
+    public void Klakson()
+    {
+        Console.WriteLine("dengan klakson tooooooooot");
+    }
+
+    public void tampilkaninfo()
+    {
+        Console.WriteLine("Mobil saya berwarna {0},merek {1} model {2} keluaran tahun {3} dengan jumlah pintu sebanyak {4}\n", Warna, Merk, Model, TahunKeluar, JumlahPintu);
+    }
+
+}
---
+++ Class1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classobj
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Mobil mobil1 = new Mobil();
+
+            mobil1.Warna = "Hitam";
+            mobil1.JumlahPintu = 2;
+            mobil1.Merk = "BMW";
+            mobil1.Model = "E30 Coupe";
+            mobil1.TahunKeluar = 1982;
+
+            mobil1.Gas(90);
+            mobil1.Klakson();
+            mobil1.tampilkaninfo();
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+}
